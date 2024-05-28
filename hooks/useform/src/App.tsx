b18c818764a674e55e2ac86ecfd1c6dbbd2b8c9d
ali baltaci <alibaltaci13@gmail.com>
2024-05-28 23:22:58 +0300

    useForm

---
+++ hooks/useform/src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <>
+      <h1>useForm + Zod</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App
---
+++ hooks/useform/src/components/Form/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react"
+
+const Form = () => {
+
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+  const [ confirmPassword, setConfirmPassword ] = useState("")
+  const [ isSubmitting, setIsSubmitting ] = useState(false)
+  const [ errors, setErrors ] = useState<string[]>([])
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsSubmitting(true)
+
+    if( password !== confirmPassword ){
+      setErrors(["Password and confirm password must match"])
+      setIsSubmitting(false)
+      return;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
+    setIsSubmitting(false)
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      {
+        errors.length > 0 && (
+          <ul>
+            {
+              errors.map( (error) => (
+                <li key={ error }>
+                  { error }
+                </li>
+               ))
+            }
+          </ul>
+        ) 
+      }
+        <input 
+          type='email' 
+          placeholder='Email' 
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value)}
+          required
+        />
+        <input 
+          type='password' 
+          placeholder='Password' 
+          value={ password }
+          onChange={ (e) => setPassword( e.target.value )}
+          required
+        />
+        <input 
+          type='password' 
+          placeholder='Confirm Password' 
+          value={ confirmPassword }
+          onChange={ (e) => setConfirmPassword( e.target.value )}
+          required
+        />
+        <button 
+          type='submit'
+          disabled={ isSubmitting }
+        >
+            Submit
+        </button>
+    </form>
+  )
+}
+
+export default Form

e7a37014b8eb121e53fd75ab40a32442cc11a722
ali baltaci <alibaltaci13@gmail.com>
2024-06-06 16:01:22 +0300

    errors

--- hooks/useform/src/components/ReactHookForm/index.tsx
+++ hooks/useform/src/components/ReactHookForm/index.tsx
@@ -13,13 +13,14 @@ const ReactHookForm = () => {
   const onSubmit = async(data: FieldValues) => {
     
     await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    reset()
   }
 
   return (
     <form onSubmit={ handleSubmit(onSubmit) }>
         <input 
-          { 
-            ...register('email',{
+          { ...register('email',{
               required: "Email is required",
             })
           }
@@ -27,6 +28,9 @@ const ReactHookForm = () => {
           placeholder='Email'
           required
         />
+        { errors.email && (
+          <p>{ `${ errors.email.message }` }</p>
+        )}
         <input 
           { ...register('password',{
             required: "Password is required",
@@ -39,15 +43,21 @@ const ReactHookForm = () => {
           placeholder='Password' 
           required
         />
+        { errors.password && (
+          <p>{ `${ errors.password.message }` }</p>
+        ) }
         <input 
           { ...register('confirmPassword',{
             required: "Confirm Pasword is required",
-
+            validate: (value) => value === getValues("password") || "Password must match"
           })}
           type='password' 
           placeholder='Confirm Password' 
           required
         />
+        { errors.confirmPassword && (
+          <p>{ `${ errors.confirmPassword.message }` }</p>
+        )}
         <button 
           type='submit'
         >
